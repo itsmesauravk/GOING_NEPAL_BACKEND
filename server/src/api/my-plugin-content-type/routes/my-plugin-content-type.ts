aa6906e23260581d569f8c2be0b4e179b29ec44b
Saurav Karki <sauravkarki10.12@gmail.com>
2024-10-04 21:05:05 +0545

    update content-type

---
+++ server/src/api/my-plugin-content-type/routes/my-plugin-content-type.ts
@@ -0,0 +1,7 @@
+/**
+ * my-plugin-content-type router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::my-plugin-content-type.my-plugin-content-type');
---
+++ server/src/api/my-plugin-content-type/services/my-plugin-content-type.ts
@@ -0,0 +1,7 @@
+/**
+ * my-plugin-content-type service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::my-plugin-content-type.my-plugin-content-type');
